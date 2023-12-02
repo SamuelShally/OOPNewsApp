6c833d78db81e5aa1ad831952c335503c9e1c4a1
SamuelShally <ss13104@nyu.edu>
2023-12-02 10:10:43 -0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Samuel Shally on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
