339df67c6dcc7cae26012d32a4271206832505e4
SamuelShally <ss13104@nyu.edu>
2023-12-10 14:01:14 -0500

    Working Tab Bar

--- NewsApp/views/ContentView.swift
+++ NewsApp/views/ContentView.swift
@@ -7,9 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View{
+    
+    @State private var selectedTabView = 1
+    
+    var body: some View{
+
+        //Add a tab bar to the bottom of the screen
+        TabView(selection: $selectedTabView){
+            ViewMain()
+                .tabItem {
+                    Image(systemName: "newspaper")
+                    Text("Articles")
+                }
+                .tag(1)
+            
+            ViewBrief()
+                .tabItem {
+                    Image(systemName: "sun.max.fill")
+                    Text("briefing")
+                }
+                .tag(2)
+            
+            ViewSettings()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("Settings")
+                }
+                .tag(3)
+        }
+        
+    }
+    
+}
+
+
+
+struct ViewMain: View {
     private var news = ApiCall()
     @State private var articlesArr: [Artical] = []
+    @State private var selectedTab = 0
     
     private func fetchData(){
         news.getTopHeadlines { [self] articles in
@@ -20,46 +57,57 @@ struct ContentView: View {
     var body: some View {
         //Navigation View to get title at the top
         NavigationView{
-            //Add a tab bar to the bottom of the screen
-            TabView{
-                //Make the Vstack below scrollable
-                ScrollView{
-                    VStack(alignment: .leading, spacing: 10) {
+            //Make the Vstack below scrollable
+            ScrollView{
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    //Display all of the articles pulled from the API
+                    ForEach(articlesArr){ article in
                         
-                        //Display all of the articles pulled from the API
-                        ForEach(articlesArr){ article in
-                            
-                            //Place each article in a rounded rectangle
-                            RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.white)
-                                .shadow(color: Color.gray.opacity(0.4), radius: 5, x: 0, y: 2)
-                                .frame(width: UIScreen.main.bounds.width, height: 200)
-                            
-                            //Overlay is the actual content
-                                .overlay(
-                                    
-                                    VStack(alignment: .leading){
-                                        Text(article.title ?? "No Headline")
-                                    }.padding()
-                                    
-                                )
-                        }
+                        //Place each article in a rounded rectangle
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .shadow(color: Color.gray.opacity(0.4), radius: 5, x: 0, y: 2)
+                            .frame(width: UIScreen.main.bounds.width, height: 200)
+                        
+                        //Overlay is the actual content
+                            .overlay(
+                                
+                                VStack(alignment: .leading){
+                                    Text(article.title ?? "No Headline")
+                                }.padding()
+                                
+                            )
                     }
                 }
-//                .padding()
-                .onAppear{
-                    fetchData()
-                }
-                
-                .tabItem {
-                    Image(systemName: "list.bullet")
-                    Text("Articles")
-                }
             }
-            .navigationBarTitle("Latest Stores")
+            .onAppear{
+                fetchData()
+            }
+        
+            .navigationBarTitle("Latest Stories")
         }
     }
 }
+
+struct ViewBrief: View{
+    var body: some View {
+            VStack {
+                Text("Hello, Brief")
+            }
+        }
+}
+
+struct ViewSettings: View{
+    var body: some View {
+            VStack {
+                Text("Hello, Settings")
+            }
+        }
+}
+
+
+
 
 #Preview {
     ContentView()
